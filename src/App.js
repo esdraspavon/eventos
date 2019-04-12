e4802da4ad2cf678107872773a2962d1a0366e59
Esdras Pavon <pavonesdras@gmail.com>
2019-04-12 10:01:18 -0300

    getting categories

--- src/App.js
+++ src/App.js
@@ -2,6 +2,29 @@ import React, { Component } from "react";
 import Header from "./components/Header";
 
 class App extends Component {
+  token = "B7GX7TMDY5ZWIQZTTWCX";
+
+  state = {
+    categories: []
+  };
+
+  componentDidMount() {
+    this.obtainCategories();
+  }
+  obtainCategories = async () => {
+    let url = `https://www.eventbriteapi.com/v3/categories/?token=${
+      this.token
+    }&locale=es_ES`;
+
+    await fetch(url)
+      .then(resp => {
+        return resp.json();
+      })
+      .then(categories => {
+        this.setState({ categories: categories.categories });
+      });
+  };
+
   render() {
     return (
       <div className="App">
